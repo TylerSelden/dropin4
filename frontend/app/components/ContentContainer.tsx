d75b37337eab469fb32a111b161d9fa6888a6245
TylerSelden <tyler.selden@salesianum.org>
2026-01-22 13:59:34 +0000

    basics

---
+++ frontend/app/components/ContentContainer.tsx
@@ -0,0 +1,7 @@
+export default function ContentContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex justify-center items-center min-h-screen p-5">
+      <div className="container p-4 bg-gray-800 rounded-lg drop-shadow-[0_0px_10px_rgba(0,0,0,0.25)] max-w-fit">{children}</div>
+    </div>
+  );
+}
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ContentContainer from "../components/ContentContainer.tsx";
+import { CiChat1 } from "react-icons/ci";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "DropIn Chat" },
+    { name: "description", content: "DropIn is a simple, web-based chat app" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <ContentContainer>
+      <div className="flex justify-center gap-2">
+        <CiChat1 className="text-violet-600 stroke-[2] text-[2.6rem]" />
+        <h1 className="bg-gradient-to-r from-violet-600 via-pink-500 to-rose-500 bg-clip-text text-transparent font-[helvetica] font-semibold text-4xl">DropIn</h1>
+      </div>
+    </ContentContainer>
+  );
 }
